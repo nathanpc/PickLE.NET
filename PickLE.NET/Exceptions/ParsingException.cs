813ee8d0280a701841dbaa6f8229ac2265fef78b
Nathan Campos <nathan@innoveworkshop.com>
2023-01-26 14:32:32 +0000

    Added a special exception for parsing issues

---
+++ PickLE.NET/Exceptions/ParsingException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PickLE.Exceptions {
+	/// <summary>
+	/// Provides detailed information about an error while parsing a document.
+	/// </summary>
+	class ParsingException : FormatException {
+		public ParsingException() : base() {
+		}
+
+		public ParsingException(string message) : base(message) {
+		}
+
+		public ParsingException(string message, Exception innerException)
+			: base(message, innerException) {
+		}
+
+		protected ParsingException(SerializationInfo info, StreamingContext context)
+			: base(info, context) {
+		}
+
+		/// <summary>
+		/// Constructs a new parsing exception with a message and the line
+		/// where the error occurred.
+		/// </summary>
+		/// <param name="message">Description of the issue.</param>
+		/// <param name="line">Line that we tried to parse.</param>
+		public ParsingException(string message, string line)
+			: this(message + "\n\tat \"" + line + "\"") {
+			Data.Add("MalformedLine", line);
+		}
+	}
+}
